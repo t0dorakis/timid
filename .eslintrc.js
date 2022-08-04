b629235e7f66999efcf83a8b72c29db56c1ea0a7
Theodor Hillmann <theo.hillmann@gmail.com>
2022-08-04 13:31:02 +0200

    no lint and better prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,17 +15,6 @@ module.exports = {
   rules: {
     'no-console': 0,
     quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        singleQuote: true,
-        printWidth: 100,
-        endOfLine: 'lf',
-        semi: false,
-        tabWidth: 2,
-      },
-    ],
   },
   plugins: ['prettier'],
-}
+};
